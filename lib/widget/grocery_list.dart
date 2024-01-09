f8b5b7c5c17570ab7cc5a363f5e6dc8986b25420
Harsh <sharshvardhan759@gmail.com>
2024-01-09 15:43:48 +0530

    Commit 1: Added groceryItem widget to the app.

---
+++ lib/widget/grocery_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems
+            .length, // so flutter knows how often the builder will be called.
+        itemBuilder: (context, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text(groceryItems[index].quantity.toString()),
+        ),
+      ),
+    );
+  }
+}
