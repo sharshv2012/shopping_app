42ef31cd8b0982f5e963b880c4b99f7a95081bce
Harsh <sharshvardhan759@gmail.com>
2024-01-10 17:28:52 +0530

    Commit 7: Added dissmisble and fallback content on the grocery items screen : )

--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -29,6 +29,33 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
+
+    Widget content = const Center(
+      child: Text("No Groceries Yet!")
+    );
+
+    if(_groceryItems.isNotEmpty){
+      content = ListView.builder(
+        itemCount: _groceryItems
+            .length, // so flutter knows how often the builder will be called.
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) => setState(() {
+            _groceryItems.removeAt(index);
+          }),
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -38,19 +65,7 @@ class _GroceryListState extends State<GroceryList> {
             onPressed: _addItem
           ),],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems
-            .length, // so flutter knows how often the builder will be called.
-        itemBuilder: (context, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(_groceryItems[index].quantity.toString()),
-        ),
-      ),
+      body: content,
     );
   }
 }
